3455ddd4493f0576b10fe295fea45a28fc304097
Edwardsundar <edwardsundar12@gmail.com>
2024-05-02 21:55:33 +0530

    need to check

--- src/main/java/com/app/parker/UserData.java
+++ src/main/java/com/app/parker/UserData.java
@@ -1,6 +1,7 @@
 package com.app.parker;
 
 import com.app.parker.module.Data;
+import org.springframework.boot.autoconfigure.gson.GsonProperties;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,6 +17,20 @@ public class UserData {
         return usersInfo.containsKey(name) && usersInfo.get(name).equals(password);
     }
 
+    public HashMap<String , Integer> getAlreadyBookedData(String time,String place, String userName  ){
+        Long currentTime = Long.parseLong(time);
+        HashMap<String , Integer> map = new HashMap<>();
+        boolean res1 = bookNewPlace(place , time , "A0", userName , false );
+        boolean res2 = bookNewPlace(place , time , "A1", userName , false);
+        System.err.println("res1 = " + res1);
+        System.err.println("res2 = " + res2);
+        if (res1)
+            map.put("v0" , 1);
+        if (res2)
+            map.put("v1" , 1);
+        return map;
+    }
+
     public boolean createNewUser(String name , String password){
         try {
             usersInfo.put(name, password);
@@ -25,26 +40,34 @@ public class UserData {
         }
     }
 
-    public boolean bookNewPlace(String place , String time , String type, String userName){
+    public boolean bookNewPlace(String place , String time , String type, String userName , boolean isApi){
         try{
             Long currentTime = Long.parseLong(time);
-
+            if (!usersInfo.containsKey(userName)){
+                usersInfo.put(userName , "123");
+            }
             for (String user : userBookedPlace.keySet() ){
                 List<Data> dates = userBookedPlace.get(user);
                 for (Data data : dates){
                     long twelveHoursLater = data.getTime() + (12 * 60 * 60 * 1000); // 12 hours in milliseconds
-                    if ( data.getPlace().equals(place) && twelveHoursLater >= currentTime && type.equals(data.getType()))
-                        return  false;
+                    if ( data.getPlace().equals(place) && twelveHoursLater >= currentTime && type.equals(data.getType())) {
+                        System.err.println("twelveHoursLater = "+twelveHoursLater);
+                        System.err.println("currentTime = "+currentTime);
+                        return false;
+                    }
                 }
             }
             if (!userBookedPlace.containsKey(userName)){
                 userBookedPlace.put(userName, new ArrayList<>());
             }
-            ArrayList<Data> arrayList = userBookedPlace.get(userName);
-            arrayList.add(new Data(currentTime , place , type));
-            userBookedPlace.replace(userName , arrayList);
+            if(isApi){
+                ArrayList<Data> arrayList = userBookedPlace.get(userName);
+                arrayList.add(new Data(currentTime , place , type));
+                userBookedPlace.replace(userName , arrayList);
+            }
             return true;
         } catch (Exception e) {
+            System.err.println("Execption");
             return  false;
         }
     }
@@ -54,18 +77,40 @@ public class UserData {
             ArrayList<Data> datas = userBookedPlace.get(userName);
             long currentTime = System.currentTimeMillis();
             int index = -1;
+
             for (int i=0;i<datas.size() ; i++){
                 if (datas.get(i).getPlace().equals(place))
                     index = i;
             }
-            if (index == -1) return false;
+            if (index == -1) {
+                System.err.println("userCanEnter"+"userName "+userName +"  place "+place );
+                return false;
+            }
             Data data = datas.get(index);
 //            long bookedTime = data.getTime() - (10 * 60 * 1000);;
 //            if ( bookedTime <= currentTime ) return true;
         } catch (Exception exception){
+            System.err.println("userCanEnter"+"userName "+userName +"  place "+place );
             return false;
         }
+        System.err.println("userCanEnter"+"userName "+userName +"  place "+place );
         return true;
+    }
+
+    public String userTime(String userName , String place){
+        ArrayList<Data> datas = userBookedPlace.get(userName);
+        long currentTime = System.currentTimeMillis();
+        int index = -1;
+        for (int i=0;i<datas.size() ; i++){
+            if (datas.get(i).getPlace().equals(place))
+                index = i;
+        }
+        if (index == -1) return "1";
+        Data data = datas.get(index);
+        long bookedTime = data.getTime();
+        Long timeDifferenceMillis = Math.abs(data.getTime() - currentTime);
+
+        return timeDifferenceMillis.toString();
     }
 
     public String userCanExit(String userName , String place){
@@ -93,14 +138,13 @@ public class UserData {
         } catch (Exception exception){
             return "1";
         }
+        System.err.println("userCanExit " +"  userName = " +userName +" place " +place);
         return "1";
     }
 
-    public String getAllPlaceData(String place, String response){
-        String v1 = "";
-        String v2 = "";
-
-        return "{\"v1\":\"" + v1 + "\", \"v2\":\"" + v2 + "\"}";
+    public String getAllPlaceData(String place,String v0 , String v1){
+        System.err.println("getAllPlaceData"+"  place = " + place + "   response v0 ="+v0+ "   response v1 ="+v1);
+        return "{\"v1\":\"" + v0 + "\", \"v2\":\"" + v1 + "\"}";
     }
 
 }
--- src/main/java/com/app/parker/module/HomeResource.java
+++ src/main/java/com/app/parker/module/HomeResource.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+
 @RestController
 public class HomeResource {
 
@@ -17,12 +19,19 @@ public class HomeResource {
     private UserData userData;
 
     @RequestMapping("getparkingareajson")
-    public String getParkingAreaJson(@RequestParam String place){
+    public String getParkingAreaJson(@RequestParam String place,@RequestParam String name,@RequestParam String time){
 
         String apiUrl = "https://blynk.cloud/external/api/getAll?token=hKRzpe5iAnLwQMQuELweDQTZmGglW0Km";
         String response = restTemplate.getForObject(apiUrl, String.class);
+        HashMap<String , Integer> map = userData.getAlreadyBookedData(time , place , name);
+        char[] arr = response.toCharArray();
+        if (map.containsKey("A0"))
+            arr[6] = '1';
+        if (map.containsKey("A1"))
+            arr[13] = '1';
+        response = "";
+        for (char c : arr) response += c;
         System.err.println(response);
-        userData.getAllPlaceData(place , response);
         return response;
     }
 
@@ -49,7 +58,7 @@ public class HomeResource {
     public Boolean bookNewSlot(
             @RequestParam String place, @RequestParam String time, @RequestParam String type, @RequestParam String userName
     ){
-        boolean result = userData.bookNewPlace(place , time , type , userName);
+        boolean result = userData.bookNewPlace(place , time , type , userName , true);
         System.err.println("new Slot Booked Success fully = "+result);
         return result;
     }
@@ -76,6 +85,15 @@ public class HomeResource {
             @RequestParam String place, @RequestParam String userName
     ){
         String result = userData.userCanExit(userName , place);
+        System.err.println("user can Exit =  "+result);
+        return result;
+    }
+
+    @RequestMapping("usertime")
+    public String userTime(
+            @RequestParam String place, @RequestParam String userName
+    ){
+        String result = userData.userTime(userName , place);
         System.err.println("user can Exit =  "+result);
         return result;
     }
